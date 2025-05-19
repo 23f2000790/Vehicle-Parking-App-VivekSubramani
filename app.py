c87464e98f5e3a6b682d50fadd0c3c2bea5e9027
vivek071512 <vivek.15.mat@gmail.com>
2025-05-19 15:38:57 +0530

    removed and relocated env, admin will be created programatically

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from application.database import db
 from application.config import LocalDevelopmentConfig
 from application.models import Users
-from flask_security import Security, SQLAlchemyUserDatastore
+from flask_security import Security, SQLAlchemyUserDatastore, hash_password
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +12,23 @@ def create_app():
     return app
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+    admin = Users.query.filter_by(type='admin').first()
+    if not admin:
+        first = Users(
+            email="admin@admin.com",
+            username="admin",
+            password="admin123",  
+            phone_no="1234567890",
+            type="admin",
+            status=True
+        )
+        db.session.add(first)
+        db.session.commit()
+
 
 if __name__ == '__main__':
     app.run()
