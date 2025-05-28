5452f1511a624d2c4344b37c108dec6f6c79f70f
23f2000790 <23f2000790@ds.study.iitm.ac.in>
2025-05-28 17:49:14 +0530

    added hash password storage

--- app.py
+++ app.py
@@ -1,34 +1,40 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from application.database import db
 from application.config import LocalDevelopmentConfig
 from application.models import Users
+from werkzeug.security import generate_password_hash
 from flask_security import Security, SQLAlchemyUserDatastore, hash_password
+
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(LocalDevelopmentConfig)
+
     db.init_app(app)
-    app.app_context().push()
+    jwt.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+        admin = Users.query.filter_by(type='admin').first()
+        if not admin:
+            first = Users(
+                email="admin@admin.com",
+                username="admin",
+                password=generate_password_hash("admin123"),  
+                phone_no="1234567890",
+                type="admin",
+                status=True
+            )
+            db.session.add(first)
+            db.session.commit()
+    a = input()
+    
     return app
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-
-    admin = Users.query.filter_by(type='admin').first()
-    if not admin:
-        first = Users(
-            email="admin@admin.com",
-            username="admin",
-            password="admin123",  
-            phone_no="1234567890",
-            type="admin",
-            status=True
-        )
-        db.session.add(first)
-        db.session.commit()
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
--- application/config.py
+++ application/config.py
@@ -8,7 +8,5 @@ class LocalDevelopmentConfig(Config):
     DEBUG = True
     #security configuration
     SECRET_KEY = 'secretkey'  #this will hash the user's information in screen
-    SECURITY_PASSWORD_HASH = 'bcrypt'  #this will hash pwd so that even developer can't see
-    SECURITY_PASSWORD_SALT = 'pwdsalt'  #helps to hash pwds
     WTF_CSRF_ENABLED = False  # helps backend make sure that data coming from a form, is actually the form of the same application
-    SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
+    JWT_SECRET_KEY = 'jwtsecretkey'
